f7ae3b8a06ebd65462b640d357395d30257a9c74
Alberto <ammalagon82@gmail.com>
2022-06-30 09:14:50 +0200

    añadida funcionalidad de saludar

---
+++ Saludar.php
@@ -0,0 +1,12 @@
+<?php
+
+class Saludar {
+    function __construct(string $nombre) {
+        $this->nombre = $nombre;
+        
+    }
+
+    function escribirSaludo() {
+        echo "Hola $this->nombre";
+    }
+}
--- index.php
+++ index.php
@@ -1,4 +1,8 @@
 <?php
-
+require_once "Saludar.php";
 echo "Hola mundo del GIT";
+
+$saludo = new Saludar("Fermin");
+
+$saludo->escribirSaludo();
 
